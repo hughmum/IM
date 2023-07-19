a1a72c809fccffbfc9c2862a2c00cd3eecf7709f
hughmum <muyubingri@163.com>
2023-07-19 12:22:08 +0800

    配置文件策略

--- im-service/src/main/java/com/mu/im/service/config/BeanConfig.java
+++ im-service/src/main/java/com/mu/im/service/config/BeanConfig.java
@@ -1,7 +1,10 @@
 package com.mu.im.service.config;
 
 import com.mu.im.common.config.AppConfig;
+import com.mu.im.common.enums.ImUrlRouteWayEnum;
+import com.mu.im.common.enums.RouteHashMethodEnum;
 import com.mu.im.common.route.RouteHandle;
+import com.mu.im.common.route.algorithm.consistenthash.AbstractConsistentHash;
 import com.mu.im.common.route.algorithm.consistenthash.ConsistentHashHandle;
 import com.mu.im.common.route.algorithm.consistenthash.TreeMapConsistentHash;
 import com.mu.im.common.route.algorithm.loop.LoopHandle;
@@ -10,6 +13,9 @@ import org.I0Itec.zkclient.ZkClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * @author Mr.yuan
@@ -30,11 +36,25 @@ public class BeanConfig {
     }
 
     @Bean
-    public RouteHandle routeHandle() {
-        ConsistentHashHandle consistentHashHandle = new ConsistentHashHandle();
-        TreeMapConsistentHash treeMapConsistentHash = new TreeMapConsistentHash();
-        consistentHashHandle.setHash(treeMapConsistentHash);
-        return consistentHashHandle;
+    public RouteHandle routeHandle() throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Integer imRouteWay = appConfig.getImRouteWay();
+        String routWay = "";
+        ImUrlRouteWayEnum handler = ImUrlRouteWayEnum.getHandler(imRouteWay);
+        routWay = handler.getClazz();
+
+        RouteHandle routeHandle = (RouteHandle) Class.forName(routWay).newInstance();
+        if (handler == ImUrlRouteWayEnum.HASH) {
+            Method setHash = Class.forName(routWay).getMethod("setHash", AbstractConsistentHash.class);
+            Integer consistentHashWay = appConfig.getConsistentHashWay();
+            String hashWay = "";
+
+            RouteHashMethodEnum hashHandler = RouteHashMethodEnum.getHandler(consistentHashWay);
+            hashWay = hashHandler.getClazz();
+            AbstractConsistentHash consistentHash
+                    = (AbstractConsistentHash) Class.forName(hashWay).newInstance();
+            setHash.invoke(routeHandle, consistentHash);
+        }
+        return routeHandle;
     }
 
 }
